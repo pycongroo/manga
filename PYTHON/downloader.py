fc88ff59c43d8daafaa5bdf188eeef9d07102815
Luis <josergp94@gmail.com>
2017-04-12 23:19:12 -0300

    Added download using pynet

--- PYTHON/downloader.py
+++ PYTHON/downloader.py
@@ -4,18 +4,18 @@ import requests
 from bs4 import BeautifulSoup
 import os
 import shutil
-import pathlib
+
+from externals.pynet import pynet as net
+
+downloader = net.NetHandler()
+dest = "Downloads"
 
 
-def cd(dir):
-    """
-    Intelligently change directory
-    """
+def create_if_not_exists(dir):
     if not os.path.exists(dir):  # check for an existing path
-        os.mkdir(path)  # make directory if it doesn't exist
-    elif not pathlib.Path(dir).is_dir():  # else check for a clashing filename
-        print("Error: A file already exists with '" + dir + "' filename")
-    os.chdir(dir)
+        os.mkdir(dir)  # make directory if it doesn't exist
+    else:
+        net.m_aviso("Path '%s' already exists" % dir)
 
 
 def get_html(url):
@@ -26,7 +26,7 @@ def get_html(url):
     return response.text  # Converts the response into text and return it
 
 if __name__ == "__main__":
-
+    create_if_not_exists(dest)
     manga = raw_input("Enter Manga name : ")
     manga = manga.strip()  # Remove extra whitespaces from the start and end of the string
     manga = manga.lower()  # Change the manga name into lowercase
@@ -35,26 +35,23 @@ if __name__ == "__main__":
     print "Enter the chapter range :"
     chaps = input("Start : ")
     chape = input("End : ")
-    dest = "Downloads"
 
-    cd(dest)
-    cd(manga)
+    path_manga = "%s/%s" % (dest, manga)
+    create_if_not_exists(path_manga)
 
     for chap in range(chaps, chape+1):
-        cd(str(chap))
-        url = "http://www.mangareader.net/"+manga+"/"+str(chap)
+        path_chap = "%s/%s" % (path_manga, chap)
+        url = "http://www.mangareader.net/%s/%s"%(manga, str(chap))
         html = get_html(url)  # HTML page of the url
 
         if "not released yet" in html:  # Checks for this string
-            print "Chapter "+str(chap)+" of "+manga+" is not available at www.mangareader.net"
-            os.chdir("..")  # Goes one directory ahead
-            shutil.rmtree(str(chap))
+            net.m_aviso("Chapter "+str(chap)+" of "+manga+" is not available at www.mangareader.net")
             break
-
+        create_if_not_exists(path_chap)
         i = 1
         while True:  # an infinte while loop
             try:
-                print "Downloading page "+str(i)+" of chapter "+str(chap)+"....."
+                net.m_aviso("Downloading page %s of chapter %s ....." % (str(i), str(chap)))
                 url = "http://www.mangareader.net/" + \
                     manga+"/"+str(chap)+"/"+str(i)
                 html = get_html(url)  # HTML page of the url
@@ -63,24 +60,22 @@ if __name__ == "__main__":
                 # Selects the suitable image tag with the link
                 ans = soup.select("#imgholder img")
                 img_link = ans[0]['src']  # Gets the image url
-                imagename = "0000"+str(i)+".jpg"
-                download_command = "wget -O "+imagename + \
-                    " -o log.txt -c "+str(img_link)
-                os.system(download_command)
-                i = i + 1
+                imagename = "0000%s.jpg" % str(i)
+                image_path = "%s/%s" % (path_chap, imagename)
+                downloader.download_file(img_link, image_path)
+                i += 1
             except:
                 break
 
-        print "Converting to pdf..."
+        net.m_accion("Converting to pdf...")
         chapno = "0000"+str(chap)
         chapno = chapno[len(chapno) - 4:]
         pdf_name = "chap"+chapno+".pdf"
-        pdf_command = "convert *.jpg ../"+pdf_name
+        path_pdf = "%s/%s" % (path_chap, pdf_name)
+        pdf_command = "convert %s/*.jpg %s" % (path_chap, path_pdf)
         os.system(pdf_command)
-        print "Cleaning up....."
+        net.m_aviso("Cleaning up.....")
         path = os.getcwd()
-        print "Your downloaded file is in this path:\n"+path
-        os.chdir("..")
-        open_command = "gnome-open "+pdf_name
+        net.m_aviso("Your downloaded file is in this path:\n %s" % path_pdf)
+        open_command = "gnome-open %s" % path_pdf
         os.system(open_command)
-        shutil.rmtree(str(chap))
